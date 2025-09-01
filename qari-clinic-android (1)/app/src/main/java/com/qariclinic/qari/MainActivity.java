197f5567b4cea060575b268e3166daa679f6e0dc
dnanHHafiz <sjsnza123@gmail.com>
2025-09-01 13:00:40 +0500

    Reorganized project - files in root

---
+++ qari-clinic-android (1)/app/src/main/java/com/qariclinic/qari/MainActivity.java
@@ -0,0 +1,19 @@
+package com.qariclinic.qari;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
+    }
+}
